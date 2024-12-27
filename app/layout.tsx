d39f03a5bae6bc07b5648c5d76f10a97b6baa9d3
Eugene Lee <yvjnlee@gmail.com>
2024-12-27 00:53:40 -0500

    changed background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
+        'text-black bg-white dark:text-white dark:bg-stone-900',
         GeistSans.variable,
         GeistMono.variable
       )}
